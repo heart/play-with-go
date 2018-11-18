91923d09c740a5f5ecf647fce4e93819bc8a09df
narongrit kanhanoi <narongrit@3dsinteractive.com>
2018-11-18 14:45:36 +0700

    refactor code

---
+++ bnk_service.go
@@ -0,0 +1,31 @@
+package main
+
+import (
+	"encoding/json"
+	"errors"
+
+	"github.com/heart/play-with-go/members"
+	"github.com/heart/play-with-go/request"
+)
+
+//BNKService
+type BNKService struct{}
+
+func NewBNKService() *BNKService {
+	return &BNKService{}
+}
+
+func (s *BNKService) getBNKMembers(r request.IRequester) ([]*members.Member, error) {
+	body, err := r.Get("https://raw.githubusercontent.com/whs/bnk48json/master/bnk48.json")
+	if err != nil {
+		return nil, errors.New("Error Request:" + err.Error())
+	}
+
+	mems := make([]*members.Member, 0)
+	err = json.Unmarshal([]byte(body), &mems)
+	if err != nil {
+		return nil, errors.New("Error JSON:" + err.Error())
+	}
+
+	return mems, nil
+}
--- main.go
+++ main.go
@@ -1,17 +1,14 @@
 package main
 
 import (
-	"encoding/json"
-	"errors"
 	"fmt"
-	"net/http"
 
-	"github.com/heart/play-with-go/members"
-	"github.com/parnurzeal/gorequest"
+	"github.com/heart/play-with-go/request"
 )
 
 func main() {
-	mems, e := getBNKMembers(NewRequester())
+	mems, e := NewBNKService().getBNKMembers(request.NewRequester())
+
 	if e == nil {
 		for _, m := range mems {
 			fmt.Println(m.ThaiFirstName, m.ThaiLastName)
@@ -19,49 +16,4 @@ func main() {
 	} else {
 		fmt.Println(e)
 	}
-}
-
-// IRequester is the interface for requester
-type IRequester interface {
-	Get(url string) (string, error)
-}
-
-// Requester is the object for requester
-type Requester struct{}
-
-// NewRequester return new requester
-func NewRequester() *Requester {
-	return &Requester{}
-}
-
-// Get the result of input url
-func (r *Requester) Get(url string) (string, error) {
-	resp, body, errs := gorequest.New().
-		Get(url).
-		End()
-
-	if len(errs) > 0 {
-		return "", errs[0]
-	}
-
-	if resp.StatusCode != http.StatusOK {
-		return body, errors.New(fmt.Sprintf("HTTP Error code=%d", resp.StatusCode))
-	}
-
-	return body, nil
-}
-
-func getBNKMembers(request IRequester) ([]*members.Member, error) {
-	body, err := request.Get("https://raw.githubusercontent.com/whs/bnk48json/master/bnk48.json")
-	if err != nil {
-		return nil, errors.New("Error Request:" + err.Error())
-	}
-
-	mems := make([]*members.Member, 0)
-	err = json.Unmarshal([]byte(body), &mems)
-	if err != nil {
-		return nil, errors.New("Error JSON:" + err.Error())
-	}
-
-	return mems, nil
 }
